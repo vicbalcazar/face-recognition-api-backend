914b5deebc3ec41c255c04fe5a17899167dde88c
vicbalcazar <victorbalcazar3@hotmail.com>
2019-04-24 10:21:42 -0700

    Refactored server.js. Refactored endpoints into controllers. The model portion of the data is  handled by Database made in Postgres

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
 const db = knex({
     client: 'pg',
     connection: {
@@ -22,126 +27,20 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            email: 'jon@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            email: 'sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-    ],
-    login: [
-        {
-            id: '987',
-            hash: '',
-            email: 'jon@gmail.com'
-        }
-    ]
-};
-
-
 app.get('/', (req, res) => {
     res.send(database.users);
     
 });
 
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    let found = false;
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
 
-    db.select('*').from('users').where({ id: id })
-        .then(user =>{
-            if (user.length) {
-                res.json(user[0]);
-            } else {
-                res.status(400).json('Not Found');
-            }
-        })
-        .catch( err => {res.status(400).json('Error getting user')})
-});
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
 
-app.post('/signin', (req, res) => {
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
 
-    //bcrypt.compare();
+app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 
-    db.select('email', 'hash').from('login')
-        .where('email', '=', req.body.email)
-        .then(data => {
-            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-            if (isValid) {
-                return db.select('*').from('users')
-                    .where('email', '=', req.body.email)
-                    .then(user => {
-                        res.json(user[0])
-                    })
-                    .catch(err => res.status(400).json('unnable to get user'))
-            } else {
-                res.status(400).json('wrong credentials');
-            }
-
-        })
-        .catch(err => res.status(400).json('wrong credentials'))
-    
-});
-
-app.post('/register', (req, res) => {
-    const {email, name, password} = req.body;
-    const hash = bcrypt.hashSync(password)
-
-    // bcrypt.hash(password, null, null, (err, hash) => {
-    //     console.log(hash);
-    // });
-        db.transaction(trx => {
-            trx.insert({
-                hash: hash,
-                email: email
-            })
-            .into('login')
-            .returning('email')
-            .then(loginEmail => {
-                return trx('users')
-                    .returning('*')
-                    .insert({
-                        email: loginEmail[0],
-                        name: name,
-                        joined: new Date()
-                    })
-                    .then (user => {
-                        res.json(user[0]);
-                    })
-            })
-            .then(trx.commit)
-            .catch(trx.rollback)
-        })
-        
-        .catch(err => res.status(400).json('unable to register'))
-
-    
-});
-
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-
-    db('users').where('id', '=', id)
-        .increment('entries', 1)
-        .returning('entries')
-        .then(entries => {
-            res.json(entries[0]);
-        })
-        .catch(err => res.status(400).json('unable to get entries'))
-   
-});
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res, db) });
 
 /*
  * /signin --> POST =success/fail
